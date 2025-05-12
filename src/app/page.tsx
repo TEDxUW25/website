825532abcbd94356b432a763c51e57591ceba1d9
joc31yn <joce.xu06@gmail.com>
2025-05-12 13:28:40 -0400

    starting timeline component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroHome from "@/components/hero";
+import Timeline from "@/components/timeline";
 
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-sans)]">
         <HeroHome />
+        <Timeline/>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
   
       </footer>
