91fcb101952f12f4ac22735c9c915e7e54e1f045
An Tran <quynhanit@gmail.com>
2025-06-06 23:48:12 -0400

    feat: disable scroll restoration on main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import HeroSection from "@/components/hero-2";
 import Timeline from "@/components/timeline";
 import Mission from "@/components/mission";
@@ -6,8 +8,15 @@ import PastTalks from "@/components/pastTalks";
 import Theme from "@/components/theme";
 import WhatIsTed from "@/components/whatIsTed";
 import ImpactPage from "./impact/page";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual';
+    }
+  }, []);
+
   return (
     <div className="font-[family-name:var(--font-sans)]">
       <HeroSection />
